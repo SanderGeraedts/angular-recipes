388879655a2f3f20a1fc999a1c77e81fdf65fd0c
Sander Geraedts <sandergeraedts@live.nl>
2017-05-27 17:51:37 +0200

    Lesson 50: Working on the ShoppingListComponent

--- src/app/shopping/list/list.component.ts
+++ src/app/shopping/list/list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients = [];
 
   constructor() { }
 
